4ef10b5dfe544bf66df7fd73d9861375d38fde49
raphaeljensen <raphaeljensen@gmail.com>
2017-06-26 14:58:05 -0700

    Add files via upload
    This search will be in case insensitive. It will return a String of contacts holding the 
    chosen last name that the user inputs. It will tell the user the number of contacts found. 
    It will be in ascending order. This method will return all contacts that match the last name 
     inputed by the user.

--- ContactList.java
+++ ContactList.java
@@ -1,43 +1,60 @@
-import java.io.Serializable;
-import java.util.ArrayList;
 
 /**
- * One object of this class represents a list contacts This class will modify,
+ * One instance of this class represents a list of contacts. This class will modify,
  * print, store, and open a contact list including first name, Last name, street
  * address, email, phone number, and notes on contact.
- * 
- * @author Donaldo
- *
  */
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner; // Imported for use case 3
 
 public class ContactList {
-	public ArrayList<Contact> allcontacts = new ArrayList<Contact>();
+	Scanner scanner = new Scanner(System.in); // for use case 3
+	private ArrayList<Contact> allcontacts = new ArrayList<Contact>();
+
+	public void addContact(Contact con) {
+		allcontacts.add(con);
+	}// end addcontact
 
 	/**
 	 * This will print entire list of contacts to console.
-	 * 
-	 * @return
 	 */
-	public String showcontactlist() {
-		return "showcontactlist() works";
+	public void showContactList() {
+		for (int i = 0; allcontacts.size() > i; i++) {
+			System.out.println(allcontacts.get(i).toString());
+		} // end of for
 	}// End of Showcontact
 
 	/**
-	 * This Method will return all contacts that match the last name inputed by
-	 * user.
-	 * 
-	 * @return
+	 * This search will be in case insensitive. It will return a String of contacts holding the 
+	 * chosen last name that the user inputs. It will tell the user the number of contacts found. 
+	 * It will be in ascending order. This method will return all contacts that match the last name 
+	 * inputed by the user.
 	 */
-
-	public String checkname() {
-		return "checkname() works";
-	}// End Checkname
+	public void searchByLastName() {
+		Collections.sort(allcontacts);
+		System.out.print("Enter last name: ");
+		String lastname = scanner.nextLine();
+		String matchingLastname = "";
+		int occurrences = 0;
+		for (int i = 0; i < allcontacts.size(); i++) {
+			Contact contact = allcontacts.get(i);
+			if (lastname.equalsIgnoreCase(contact.getLastName())) {
+				matchingLastname += contact + "\n";
+				occurrences++;
+			}
+		}
+		if (matchingLastname.isEmpty()) {
+			System.out.println("No Contacts with that last name were found. \n");
+		} else {
+			System.out.println("=== " + occurrences + " Contacts Found! === \n\n" + matchingLastname);
+		}
+	}// End searchByLastName
 
 	/**
 	 * This method will save contact list to a file on hard disk.
 	 */
 	public void save() {
-
 	}// End Save
 
 	/**
@@ -48,11 +65,4 @@ public class ContactList {
 
 	}// End of open
 
-	/**
-	 * This will return the list of contacts as a string.
-	 */
-	public String toString() {
-		return "toString() works";
-	}// End of toString
-
-}// End of class
+}
